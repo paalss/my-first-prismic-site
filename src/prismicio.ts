65295db1c1f5d9101c48409a674cd2250cba468f
paalss <paalss94@gmail.com>
2024-06-23 17:50:21 +0200

    feat: add more routes

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -14,6 +14,8 @@ export const repositoryName =
 const routes: prismic.ClientConfig["routes"] = [
   { type: "page", path: "/", uid: "home" },
   { type: "page", path: "/:uid" },
+  { type: "page", path: "/products" },
+  { type: "page", path: "/products:uid" },
 ];
 
 /**
